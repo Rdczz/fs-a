b6e11af3cd598b038521dce9289b18deaa902287
Rdczz <anjanirohith@gmail.com>
2022-01-20 20:57:45 +0530

    day19-p1 sol

--- day19-p1.cpp
+++ day19-p1.cpp
@@ -25,3 +25,22 @@ output =
 
 
  */
+#include <bits/stdc++.h>
+using namespace std;
+void pattern(int n){
+    int current=1,temp=1,result;
+    for(int i=0;i<n;i++){
+        for(int j=0;j<=i;j++){
+            result=current+temp;
+            cout<<current<<" ";
+            current=temp;
+            temp=result;
+        }cout<<endl;
+    }
+}
+int main(){
+    int n;
+    cin>>n;
+    pattern(n);
+    return 0;
+}
